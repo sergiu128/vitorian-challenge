555b87c3b1cee2e81e83f0511d6f61d8c0017d8c
sergiu128 <sergiu128@protonmail.com>
2023-11-09 21:38:24 +0100

    Implement and test login request

---
+++ msg_login_request.hpp
@@ -0,0 +1,96 @@
+#pragma once
+
+#include <cstddef>
+#include <cstdint>
+#include <cstdlib>
+#include <cstring>
+#include <stdexcept>
+
+class LoginRequest {
+ public:
+  LoginRequest() = delete;
+  LoginRequest(char *buffer, size_t buffer_length, size_t buffer_offset)
+      : buffer_{buffer},
+        buffer_length_{buffer_length},
+        buffer_offset_{buffer_offset} {
+    if (buffer_length < buffer_offset + EncodedLength()) {
+      throw std::runtime_error("buffer to small for LoginRequest");
+    }
+  }
+  ~LoginRequest() = default;
+
+  // Copy.
+  LoginRequest(const LoginRequest &) = default;
+  LoginRequest &operator=(const LoginRequest &) = default;
+
+  // Move.
+  LoginRequest(LoginRequest &&) = default;
+  LoginRequest &operator=(LoginRequest &&) = default;
+
+  [[nodiscard]] char MsgType() const noexcept { return 'L'; }
+
+  [[nodiscard]] static constexpr size_t EncodedLength() noexcept { return 96; }
+
+  [[nodiscard]] const char *Buffer() const noexcept { return buffer_; }
+  [[nodiscard]] size_t BufferLength() const noexcept { return buffer_length_; }
+  [[nodiscard]] size_t BufferOffset() const noexcept { return buffer_offset_; }
+
+  // User: text, 64 bytes, offset 0
+  [[nodiscard]] static constexpr size_t UserEncodingLength() noexcept {
+    return 64;
+  }
+
+  [[nodiscard]] static constexpr size_t UserEncodingOffset() noexcept {
+    return 0;
+  }
+
+  [[nodiscard]] const char *User() const noexcept {
+    return buffer_ + buffer_offset_ + UserEncodingOffset();
+  }
+
+  template <typename ClaimFnT>
+  LoginRequest &User(ClaimFnT &&fn) noexcept {
+    char *b = buffer_ + buffer_offset_ + UserEncodingOffset();
+    size_t max_length = UserEncodingLength() - 1;
+    size_t n_used = fn(b, max_length);
+
+    if (n_used > max_length) {
+      n_used = max_length;
+    }
+    *(b + n_used) = '\0';
+
+    return *this;
+  }
+
+  // Password: text, 32 bytes; offset 64
+  [[nodiscard]] static constexpr size_t PasswordEncodingLength() noexcept {
+    return 32;
+  }
+
+  [[nodiscard]] static constexpr size_t PasswordEncodingOffset() noexcept {
+    return 64;
+  }
+
+  [[nodiscard]] const char *Password() const noexcept {
+    return buffer_ + buffer_offset_ + PasswordEncodingOffset();
+  }
+
+  template <typename ClaimFnT>
+  LoginRequest &Password(ClaimFnT &&fn) noexcept {
+    char *b = buffer_ + buffer_offset_ + PasswordEncodingOffset();
+    size_t max_length = PasswordEncodingLength() - 1;
+
+    size_t n_used = fn(b, max_length);
+    if (n_used > max_length) {
+      n_used = max_length;
+    }
+    *(b + n_used) = '\0';
+
+    return *this;
+  }
+
+ private:
+  char *buffer_;
+  size_t buffer_length_;
+  size_t buffer_offset_;
+};
--- test.cpp
+++ test.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 
 #include "msg_header.hpp"
+#include "msg_login_request.hpp"
 
 void TestMsgHeader() {
   // Ensure encoding/decoding works.
@@ -50,7 +51,105 @@ void TestMsgHeader() {
   std::cout << "TestMsgHeader done." << std::endl;
 }
 
+void TestLoginRequest() {
+  // Ensure encoding/decoding works.
+  {
+    char buf[128];
+    LoginRequest req{buf, 128, 0};
+
+    assert((void*)req.Buffer() == (void*)buf);
+    assert(req.BufferLength() == 128);
+    assert(req.BufferOffset() == 0);
+    assert(req.EncodedLength() == 96);
+
+    assert(req.UserEncodingLength() == 64);
+    assert(req.UserEncodingOffset() == 0);
+
+    assert(req.PasswordEncodingLength() == 32);
+    assert(req.PasswordEncodingOffset() == 64);
+
+    assert(req.MsgType() == 'L');
+
+    // Encode and check decoding
+    const char* user = "sergiu4096@gmail.com";
+    size_t user_length = strlen(user);
+
+    const char* password = "pwd123";
+    size_t password_length = strlen(password);
+
+    req.User([&](char* slice, size_t max_length) {
+         assert(max_length == LoginRequest::UserEncodingLength() - 1);
+         assert((void*)slice == (void*)((char*)buf + req.UserEncodingOffset()));
+
+         memcpy(slice, user, user_length);
+         return user_length;
+       })
+        .Password([&](char* slice, size_t len) {
+          assert(len == req.PasswordEncodingLength() - 1);
+          assert((void*)slice ==
+                 (void*)((char*)buf + req.PasswordEncodingOffset()));
+
+          memcpy(slice, password, password_length);
+          return password_length;
+        });
+    assert(strcmp(req.User(), user) == 0);
+    assert(strcmp(req.Password(), password) == 0);
+  }
+
+  // Ensure the message can fit in the passed buffer.
+  {
+    constexpr size_t InvalidLength = LoginRequest::EncodedLength() - 1;
+    char buf[InvalidLength];
+    bool ok{false};
+    try {
+      LoginRequest req{buf, InvalidLength, 0};
+    } catch (const std::exception& e) {
+      ok = true;
+    }
+    assert(ok);
+  }
+
+  // Ensure user is truncated if it exceeds its max length.
+  {
+    constexpr size_t Length = LoginRequest::EncodedLength() * 2;
+    char buf[Length];
+    memset(buf, 0, Length);
+    LoginRequest req{buf, Length, 0};
+
+    req.User([&](char* slice, size_t max_length) {
+      for (size_t i = 0; i < max_length + 2; i++) slice[i] = 'a';
+      return max_length + 2;
+    });
+    for (size_t i = LoginRequest::UserEncodingOffset();
+         i < LoginRequest::UserEncodingLength() - 1; i++) {
+      assert(buf[i] == 'a');
+    }
+    assert(buf[LoginRequest::UserEncodingLength() - 1] == '\0');
+  }
+
+  // Ensure user is truncated if it exceeds its max length.
+  {
+    constexpr size_t Length = LoginRequest::EncodedLength() * 2;
+    char buf[Length];
+    memset(buf, 0, Length);
+    LoginRequest req{buf, Length, 0};
+
+    req.Password([&](char* slice, size_t max_length) {
+      for (size_t i = 0; i < max_length + 2; i++) slice[i] = 'b';
+      return max_length + 2;
+    });
+    for (size_t i = LoginRequest::PasswordEncodingOffset();
+         i < LoginRequest::PasswordEncodingLength() - 1; i++) {
+      assert(buf[i] == 'b');
+    }
+    assert(buf[LoginRequest::PasswordEncodingLength() - 1] == '\0');
+  }
+
+  std::cout << "TestLoginRequest done." << std::endl;
+}
+
 int main() {
   TestMsgHeader();
+  TestLoginRequest();
   std::cout << "Bye." << std::endl;
 }
