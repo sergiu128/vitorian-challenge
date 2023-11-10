cc88c4d9b67aa5b03095daffc3d372f4197fb65f
sergiu128 <sergiu128@protonmail.com>
2023-11-10 14:59:57 +0100

    Add checksum verification

---
+++ checksum.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <cstdint>
+
+inline uint16_t VerifyChecksum(const char* b, uint32_t len) {
+  uint32_t sum = 0;
+  for (uint32_t j = 0; j < len - 1; j += 2) {
+    sum += *((uint16_t*)(&b[j]));
+  }
+  if ((len & 1) != 0) {
+    sum += b[len - 1];
+  }
+  sum = (sum >> 16) + (sum & 0xFFFF);
+  sum = (sum >> 16) + (sum & 0xFFFF);
+  return uint16_t(~sum);
+}
